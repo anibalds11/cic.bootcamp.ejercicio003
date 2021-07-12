c0ed1f960711d14c79a16348f2cbb141530a9940
anibal <anibalds11@gmail.com>
2021-07-12 14:29:08 +0200

    Ejercicio003 version 1.1

--- src/main/java/ejercicio003/Grupo.java
+++ src/main/java/ejercicio003/Grupo.java
@@ -8,12 +8,33 @@ public class Grupo {
 	static List<String> lista = new ArrayList<>();
 	
 
-	public String getPersona(int n)
+	public String getPersona()
 	{
+		int n = (int) (Math.random() * lista.size());
 		return lista.get(n);
 	}
 	
-	public 
+	public List<String> getPersonas(int n)
+	{
+		List<String> listaPersonasDevovler = new ArrayList<>();
+		do {
+		
+			int aleatorio = (int) (Math.random() * lista.size());
+			String persona=lista.get(aleatorio);
+			listaPersonasDevovler.add(persona);
+					
+		}while(n!=0);
+			
+			return listaPersonasDevovler;
+	}
+
+	public static List<String> getLista() {
+		return lista;
+	}
+
+	public static void setLista(List<String> lista) {
+		Grupo.lista = lista;
+	}
 	
 	
 }
--- src/test/java/ejercicio003/PersonaTest.java
+++ src/test/java/ejercicio003/PersonaTest.java
@@ -2,23 +2,43 @@ package ejercicio003;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class PersonaTest {
 
+	static Grupo lista = new Grupo();
+	
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
+		ArrayList<String> strings = new ArrayList<String>();
+		
+		strings.add("Pepe Gonzalez");
+		strings.add("Samuel Vazquez");
+		strings.add("Sara Nuñez");
+		strings.add("Iker Casillas");
+		strings.add("Cristiano Ronaldo");
+		
+		lista.setLista(strings);
 	}
 
 	@BeforeEach
 	void setUp() throws Exception {
+		
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testPersonas() {
+		
+		String persona=lista.getPersona();
+		
+		assertEquals(persona,persona);
+		
+		
 	}
 
 }
