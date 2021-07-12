4cb56bdd288be0bdd5b55f5e78a6bd08011f43e2
anibal <anibalds11@gmail.com>
2021-07-12 14:11:38 +0200

    Ejercicio003 version1.0

---
+++ src/main/java/ejercicio003/Grupo.java
@@ -0,0 +1,19 @@
+package ejercicio003;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Grupo {
+
+	static List<String> lista = new ArrayList<>();
+	
+
+	public String getPersona(int n)
+	{
+		return lista.get(n);
+	}
+	
+	public 
+	
+	
+}
